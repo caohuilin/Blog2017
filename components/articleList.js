d66cc201a754968016ed3dc77ef8e89615614caf
caohuilin <caohuilin9588@foxmail.com>
2017-11-25 16:04:59 +0800

    将钢丝模糊样式移动到css

--- components/articleList.js
+++ components/articleList.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
+import classnames from 'classnames';
 import { Motion, spring } from 'react-motion';
 import { inject, observer } from 'mobx-react';
 import * as _ from 'lodash';
@@ -9,28 +10,20 @@ import * as _ from 'lodash';
 class Article extends React.Component {
   render() {
     const { showSelectMenu } = this.props.store;
-    const blurStyle = showSelectMenu
-    ? {
-        filter: `blur(5px)`,
-        transition: `0.3s filter linear`
-      }
-    : {
-      filter: `blur(0px)`,
-      transition: `0.1s filter linear`
-    };
+    const blurClass = classnames({blur: showSelectMenu, unblur: !showSelectMenu})
     return (
       <nav className="content">
         <ul>
           <li>
             <Link href={{ pathname: '/article', query: { id: 0 } }}>
-              <h3 className="title" style={blurStyle}>谈谈阿里的前端笔试题<span /></h3>
+              <h3 className={`title ${blurClass}`}>谈谈阿里的前端笔试题<span /></h3>
             </Link>
-            <div className="introduction" style={blurStyle}>
+            <div className={`introduction ${blurClass}`}>
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
             </div>
-            <div className="information" style={blurStyle}>
+            <div className={`information ${blurClass}`}>
               <div className="like">
                 <i className="iconfont">&#xe60b;</i>
               </div>
@@ -39,14 +32,14 @@ class Article extends React.Component {
           </li>
           <li>
             <Link href={{ pathname: '/article', query: { id: 1 } }}>
-              <h3 className="title" style={blurStyle}>搞怪的JavaScript数据类型<span /></h3>
+              <h3 className={`title ${blurClass}`}>搞怪的JavaScript数据类型<span /></h3>
             </Link>
-            <div className="introduction" style={blurStyle}>
+            <div className={`introduction ${blurClass}`}>
               js中数据类型方面应该注意的一些问题
               js中数据类型方面应该注意的一些问题
               js中数据类型方面应该注意的一些问题
             </div>
-            <div className="information" style={blurStyle}>
+            <div className={`information ${blurClass}`}>
               <div className="like">
                 <i className="iconfont">&#xe60b;</i>
               </div>
@@ -55,14 +48,14 @@ class Article extends React.Component {
           </li>
           <li>
             <Link href={{ pathname: '/article', query: { id: 2 } }}>
-              <h3 className="title" style={blurStyle}>谈谈阿里的前端笔试题<span /></h3>
+              <h3 className={`title ${blurClass}`}>谈谈阿里的前端笔试题<span /></h3>
             </Link>
-            <div className="introduction" style={blurStyle}>
+            <div className={`introduction ${blurClass}`}>
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
               前几天做了阿里实习生的前端笔试题，和大家分享一下。
             </div>
-            <div className="information" style={blurStyle}>
+            <div className={`information ${blurClass}`}>
               <div className="like">
                 <i className="iconfont">&#xe60b;</i>
               </div>
@@ -71,14 +64,14 @@ class Article extends React.Component {
           </li>
           <li>
             <Link href={{ pathname: '/article', query: { id: 3 } }}>
-              <h3 className="title" style={blurStyle}>搞怪的JavaScript数据类型<span /></h3>
+              <h3 className={`title ${blurClass}`}>搞怪的JavaScript数据类型<span /></h3>
             </Link>
-            <div className="introduction" style={blurStyle}>
+            <div className={`introduction ${blurClass}`}>
               js中数据类型方面应该注意的一些问题
               js中数据类型方面应该注意的一些问题
               js中数据类型方面应该注意的一些问题
             </div>
-            <div className="information" style={blurStyle}>
+            <div className={`information ${blurClass}`}>
               <div className="like">
                 <i className="iconfont">&#xe60b;</i>
               </div>
