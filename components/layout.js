b8d26bebd8f603e4f02eff20714b4f6ecf3d1c97
caohuilin <caohuilin9588@foxmail.com>
2017-11-27 23:08:59 +0800

    link 新页面打开

--- components/layout.js
+++ components/layout.js
@@ -1,47 +1,47 @@
-import Head from 'next/head'
-import React from 'react'
-import { inject, observer } from 'mobx-react'
-import Link from 'next/link'
-import reset from '../styles/reset.css'
-import layout from '../styles/layout.css'
+import Head from 'next/head';
+import React from 'react';
+import Link from 'next/link';
+import { inject, observer } from 'mobx-react';
+import reset from '../styles/reset.css';
+import layout from '../styles/layout.css';
 
 @inject('store')
 @observer
 export default class Layout extends React.Component {
-  handleWheel = (e) => {
+  handleWheel = e => {
     if (this.props.store.disableScroll) {
-      e.preventDefault()
+      e.preventDefault();
     }
-  }
+  };
 
-  handleKeyDown = (e) => {
+  handleKeyDown = e => {
     if (this.props.store.disableScroll) {
-      e.preventDefault()
+      e.preventDefault();
     }
-  }
+  };
 
   componentDidMount() {
-    document.body.addEventListener('wheel', this.handleWheel)
-    document.body.addEventListener('keydown', this.handleKeyDown)
+    document.body.addEventListener('wheel', this.handleWheel);
+    document.body.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.body.removeEventListener('wheel', this.handleWheel)
-    document.body.removeEventListener('keydown', this.handleKeyDown)
+    document.body.removeEventListener('wheel', this.handleWheel);
+    document.body.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render() {
-    const { title, children } = this.props
-    const { showSelectMenu } = this.props.store
+    const { title, children } = this.props;
+    const { showSelectMenu } = this.props.store;
     const blurStyle = showSelectMenu
-    ? {
-        filter: `blur(10px)`,
-        transition: `0.3s filter linear`
-      }
-    : {
-      filter: `blur(0px)`,
-      transition: `0.3s filter linear`
-    }
+      ? {
+          filter: `blur(10px)`,
+          transition: `0.3s filter linear`
+        }
+      : {
+          filter: `blur(0px)`,
+          transition: `0.3s filter linear`
+        };
     return (
       <div>
         <Head>
@@ -57,7 +57,9 @@ export default class Layout extends React.Component {
           <style dangerouslySetInnerHTML={{ __html: layout }} />
           <div className="page">
             <header>
-              <div id="logo" style={blurStyle}>=CHL</div>
+              <div id="logo" style={blurStyle}>
+                =CHL
+              </div>
               <nav>
                 <ul>
                   <li style={blurStyle}>
@@ -66,14 +68,22 @@ export default class Layout extends React.Component {
                     </Link>
                   </li>
                   <li style={blurStyle}>
-                    <Link href="https://hi-hi.cn/chlresume/">
-                      <a className="nav-link">Resume</a>
-                    </Link>
+                    <a
+                      href="https://hi-hi.cn/chlresume/"
+                      target="_blank"
+                      className="nav-link"
+                    >
+                      Resume
+                    </a>
                   </li>
                   <li style={blurStyle}>
-                    <Link href="https://github.com/caohuilin">
-                      <a className="nav-link">GitHub</a>
-                    </Link>
+                    <a
+                      href="https://github.com/caohuilin"
+                      target="_blank"
+                      className="nav-link"
+                    >
+                      GitHub
+                    </a>
                   </li>
                 </ul>
               </nav>
@@ -82,6 +92,6 @@ export default class Layout extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
